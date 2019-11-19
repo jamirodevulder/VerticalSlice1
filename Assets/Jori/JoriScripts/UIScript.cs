8c4aa67892634df20e05fea411bec3e11c6d2cb7
Jorikos <26204@ma-web.nl>
2019-11-19 09:19:46 +0100

    Pauzeknop positie veranderd, comments

--- Assets/Jori/JoriScripts/UIScript.cs
+++ Assets/Jori/JoriScripts/UIScript.cs
@@ -5,19 +5,29 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    bool unpaused = true;
-    Text buttonText;
-    Button pauseButton;
-    Camera mainCamera;
+    private bool unpaused = true; //unpaused true is dat het spel niet gepauzeerd is, unpaused false is gepauzeerd
+    private Text buttonText;
+    private Button pauseButton;
+    private Camera mainCamera;
+    private Vector3 returnPosition;
+
     private void Awake()
     {
         pauseButton = GetComponent<Button>();
-        GetComponent<Button>().onClick.AddListener(onButtonClick); //Pakt de onClick function van de Button
+        GetComponent<Button>().onClick.AddListener(OnButtonClick); //Pakt de onClick function van de button
         buttonText = GetComponentInChildren<Text>();
         buttonText.text = "Pauzeren";
-
+        mainCamera = FindObjectOfType<Camera>();
+        returnPosition = pauseButton.transform.position; //Neemt huidige positie van de pauzeknop
     }
-    void onButtonClick()
+    private void Update()
+    {
+        if (unpaused)
+        {
+            returnPosition = pauseButton.transform.position; //Neemt huidige positie van de pauzeknop wanneer het niet gepauzeerd is, als de camera beweegt blijft de positie hetzelfde
+        }
+    }
+    private void OnButtonClick()
     {
         switch (unpaused)
         {
@@ -25,12 +35,14 @@ public class UIScript : MonoBehaviour
                 unpaused = false;
                 Time.timeScale = 0; //Zet op pause
                 buttonText.text = "Spel hervatten";
+                pauseButton.transform.position = new Vector3(mainCamera.transform.position.x,mainCamera.transform.position.y,0); //Z positie wordt anders -400
                 break;
 
             case false:
                 unpaused = true;
                 Time.timeScale = 1; //Zet weer aan
                 buttonText.text = "Pauzeren";
+                pauseButton.transform.position = returnPosition;
                 break;
         }
     }
