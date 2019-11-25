ae855f67df2382ee5363e072d656516ebb1f9a97
Jorikos <26204@ma-web.nl>
2019-11-25 11:48:10 +0100

    Scorescript

--- Assets/Jori/JoriScripts/Move.cs
+++ Assets/Jori/JoriScripts/Move.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    private ScoreScript scoreScript;
+    private Sprite enemy;
+    
+    private void Awake()
+    {
+        enemy = FindObjectOfType<Sprite>();
+    }
+
     private void Update()
     {
         transform.Translate(Vector3.right * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            scoreScript.AddScore(10);
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Assets/Jori/JoriScripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private float score;
+    private Text scoreText;
+    [SerializeField] private GameObject enemyt;
+
+    private void Awake()
+    {
+        enemyt = GameObject.Find("Enemy");
+        Debug.Log(enemyt);
+        score = 0;
+        scoreText = GetComponent<Text>();
+        scoreText.text = "Score: " + score;
+    }
+
+
+    public void AddScore(float tempScore)
+    {
+        score += tempScore;
+        scoreText.text = "Score: " + score;
+    }
+}
