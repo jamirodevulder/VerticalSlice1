11ba32b10845048afb5d7d0d31719ab0171e10f3
Jorikos <26204@ma-web.nl>
2019-11-25 09:14:51 +0100

    Level select/Main menu placeholder
    Er staat één knop in om terug te gaan naar JoriScene

---
+++ Assets/Jori/JoriScripts/Return.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Return : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponentInChildren<Button>().onClick.AddListener(ReturnKnop);
+    }
+
+    private void ReturnKnop()
+    {
+        SceneManager.LoadScene("JoriScene");
+    }
+}
